6af068e6ca2238dc2977c318c4effa20332b4c71
MjiaAndy <armmcarp123@gmail.com>
2025-09-11 03:50:53 -0300

    deploy-fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Conecta a la base de datos
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB Atlas'))
-  .catch(err => console.error('Error al conectar con la base de datos:', err));
+  .catch(err => console.error('Error al conectar:', err));
 
 app.use(cors());
 app.use(express.json());
